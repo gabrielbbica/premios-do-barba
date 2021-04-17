f1deadda0840919ceac4c86ebd5ffc3356d74f01
gabrielbbica <61632492+gabrielbbica@users.noreply.github.com>
2021-04-17 16:50:29 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/all.min.css">
+    <link rel="stylesheet" href="css/site.css">
+
+    <title>Home</title>
+  </head>
+</head>
+<body class="bg-dark">
+	<?php include('inc/parts/header.php'); ?>
+	<?php include('inc/parts/banner.php'); ?>
+	<?php include('inc/parts/proximos-sorteios.php'); ?>
+	<?php include('inc/parts/ganhadores.php'); ?>
+	<?php include('inc/parts/parceiros.php'); ?>
+	<?php include('inc/parts/footer.php'); ?>
+
+	<script type="text/javascript" src="js/jquery-3.6.0.min.js"></script>
+	<script type="text/javascript" src="js/slick.min.js"></script>
+	<script type="text/javascript" src="js/functions.js"></script>
+</body>
+</html>
